a5f8b7b19f2d806260609df39ad77ac3293c7824
surey <komeijiddz@gmail.com>
2024-06-07 20:07:34 +0800

    - venv

--- .venv/bin/analyzer.py
+++
@@ -1,7 +0,0 @@
-#!/home/shuo/repo/evaluation/.venv/bin/python3
-# EASY-INSTALL-DEV-SCRIPT: 'eTainter==0.1.0','analyzer.py'
-__requires__ = 'eTainter==0.1.0'
-__import__('pkg_resources').require('eTainter==0.1.0')
-__file__ = '/home/shuo/repo/evaluation/bin/analyzer.py'
-with open(__file__) as f:
-    exec(compile(f.read(), __file__, 'exec'))
