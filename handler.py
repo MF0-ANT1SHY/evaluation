f127f2c87ec5719476b9997c9f5bc0f37a142fc7
Shuo Yang <103630612+MF0-ANT1SHY@users.noreply.github.com>
2024-06-12 17:43:08 +0800

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,15 +1,22 @@
 import os
-import glob
 import subprocess
 from multiprocessing import Pool
 import psutil
-import time
+from src.util.logmanager import setuplogger
+import csv
 
 
-def run_process(file):
+def run_process(file, timeoutsize=2 * 60):
+    logger = setuplogger()
+    name = file.rsplit("/", 1)[-1]
     """运行单个处理进程"""
-    cmd = f"python bin/analyzer.py -f {file} -b "
-    subprocess.run(cmd, shell=True)
+    cmd = ["python3", "bin/analyzer.py", "-f", file, "-b"]
+    try:
+        subprocess.run(cmd, timeout=timeoutsize)
+    except subprocess.TimeoutExpired:
+        logger.info(
+            f"{name},{None},{True},{None},{None},{None},{None},{None},{None},{timeoutsize},{None},{None}"
+        )
 
 
 def main():
@@ -27,25 +34,46 @@ def main():
     num_processes = min(max_processes_by_memory, max_processes_by_cpu)
 
     print(f"Starting {num_processes} processes...")
-    time.sleep(5)
 
     directories = [
         os.path.expanduser("/home/shuo/repo/TaintStateMachine/datasets/TN"),
     ]
 
+    archivefile = "archivelist.csv"
+
+    timeoutlimit = 2 * 60
+
+    analyzed_filenames = set()
+    with open(archivefile, newline="") as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if len(row) > 3:  # 确保行有足够的列
+                analyzed_filenames.add(row[4].strip())
+
     pool = Pool(num_processes)
     print(pool._cache)
+    print(f"{len(analyzed_filenames)} files have been analyzed before.")
 
     filelist = []
 
+    # 遍历目录中的文件
     for directory_path in directories:
         for filename in os.listdir(directory_path):
             if filename.endswith(".code"):
+                # 检查文件是否已经被分析过
+                if filename in analyzed_filenames:
+                    continue  # 已分析过的文件跳过不处理
                 full_path = os.path.join(directory_path, filename)
                 filelist.append(full_path)
 
     for path in filelist:
-        pool.apply_async(run_process, (path,))
+        pool.apply_async(
+            run_process,
+            (
+                path,
+                timeoutlimit,
+            ),
+        )
     pool.close()
     pool.join()
 
